2ecb5fa06bb780e171c39b0ed6b2f43dfb33fffd
Jonathan Esterhazy <esterhaz@amazon.com>
2017-11-03 11:23:10 -0700

    add mxnet-gluon-cifar10 example

---
+++ im-python-sdk/mxnet-gluon-cifar10/credentials.py
@@ -0,0 +1,5 @@
+import os
+
+# uncomment these lines and add your credentials
+# os.environ['AWS_ACCESS_KEY_ID'] = 'type your aws access key id here'
+# os.environ['AWS_SECRET_ACCESS_KEY'] = 'type your aws secret access key here'
