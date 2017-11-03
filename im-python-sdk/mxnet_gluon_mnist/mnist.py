ae4df9a79376c6c15d7d50cf6710d864054c3ef7
Jonathan Esterhazy <esterhaz@amazon.com>
2017-11-03 11:35:57 -0700

    add mxnet_gluon_mnist example

---
+++ im-python-sdk/mxnet_gluon_mnist/mnist.py
@@ -0,0 +1,159 @@
+from __future__ import print_function
+
+import logging
+import mxnet as mx
+from mxnet import gluon, autograd
+from mxnet.gluon import nn
+import numpy as np
+import json
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# ------------------------------------------------------------ #
+# Training methods                                             #
+# ------------------------------------------------------------ #
+
+
+def train(channel_input_dirs, hyperparameters, **kwargs):
+    # IM passes num_cpus, num_gpus and other args we can use to tailor training to
+    # the current container environment, but here we just use simple cpu context.
+    ctx = mx.cpu()
+
+    # retrieve the hyperparameters we set in notebook (with some defaults)
+    batch_size = hyperparameters.get('batch_size', 100)
+    epochs = hyperparameters.get('epochs', 10)
+    learning_rate = hyperparameters.get('learning_rate', 0.1)
+    momentum = hyperparameters.get('momentum', 0.9)
+    log_interval = hyperparameters.get('log_interval', 100)
+
+    # load training and validation data
+    # we use the gluon.data.vision.MNIST class because of its built in mnist pre-processing logic,
+    # but point it at the location where IM placed the data files, so it doesn't download them again.
+    training_dir = channel_input_dirs['training']
+    train_data = get_train_data(training_dir + '/train', batch_size)
+    val_data = get_val_data(training_dir + '/test', batch_size)
+
+    # define the network
+    net = define_network()
+
+    # Collect all parameters from net and its children, then initialize them.
+    net.initialize(mx.init.Xavier(magnitude=2.24), ctx=ctx)
+    # Trainer is for updating parameters with gradient.
+    trainer = gluon.Trainer(net.collect_params(), 'sgd',
+                            {'learning_rate': learning_rate, 'momentum': momentum})
+    metric = mx.metric.Accuracy()
+    loss = gluon.loss.SoftmaxCrossEntropyLoss()
+
+    for epoch in range(epochs):
+        # reset data iterator and metric at begining of epoch.
+        metric.reset()
+        for i, (data, label) in enumerate(train_data):
+            # Copy data to ctx if necessary
+            data = data.as_in_context(ctx)
+            label = label.as_in_context(ctx)
+            # Start recording computation graph with record() section.
+            # Recorded graphs can then be differentiated with backward.
+            with autograd.record():
+                output = net(data)
+                L = loss(output, label)
+                L.backward()
+            # take a gradient step with batch_size equal to data.shape[0]
+            trainer.step(data.shape[0])
+            # update metric at last.
+            metric.update([label], [output])
+
+            if i % log_interval == 0 and i > 0:
+                name, acc = metric.get()
+                print('[Epoch %d Batch %d] Training: %s=%f' % (epoch, i, name, acc))
+
+        name, acc = metric.get()
+        print('[Epoch %d] Training: %s=%f' % (epoch, name, acc))
+
+        name, val_acc = test(ctx, net, val_data)
+        print('[Epoch %d] Validation: %s=%f' % (epoch, name, val_acc))
+
+    return net
+
+
+def save(net, model_dir):
+    # save the model
+    y = net(mx.sym.var('data'))
+    y.save('%s/model.json' % model_dir)
+    net.collect_params().save('%s/model.params' % model_dir)
+
+
+def define_network():
+    net = nn.Sequential()
+    with net.name_scope():
+        net.add(nn.Dense(128, activation='relu'))
+        net.add(nn.Dense(64, activation='relu'))
+        net.add(nn.Dense(10))
+    return net
+
+
+def input_transformer(data, label):
+    data = data.reshape((-1,)).astype(np.float32) / 255
+    return data, label
+
+
+def get_train_data(data_dir, batch_size):
+    return gluon.data.DataLoader(
+        gluon.data.vision.MNIST(data_dir, train=True, transform=input_transformer),
+        batch_size=batch_size, shuffle=True, last_batch='discard')
+
+
+def get_val_data(data_dir, batch_size):
+    return gluon.data.DataLoader(
+        gluon.data.vision.MNIST(data_dir, train=False, transform=input_transformer),
+        batch_size=batch_size, shuffle=False)
+
+
+def test(ctx, net, val_data):
+    metric = mx.metric.Accuracy()
+    for data, label in val_data:
+        data = data.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+        output = net(data)
+        metric.update([label], [output])
+    return metric.get()
+
+
+# ------------------------------------------------------------ #
+# Hosting methods                                              #
+# ------------------------------------------------------------ #
+
+def model_fn(model_dir):
+    """
+    Load the gluon model. Called once when hosting service starts.
+
+    :param: model_dir The directory where model files are stored.
+    :return: a model (in this case a Gluon network)
+    """
+    symbol = mx.sym.load('%s/model.json' % model_dir)
+    outputs = mx.symbol.softmax(data=symbol, name='softmax_label')
+    inputs = mx.sym.var('data')
+    param_dict = gluon.ParameterDict('model_')
+    net = gluon.SymbolBlock(outputs, inputs, param_dict)
+    net.load_params('%s/model.params' % model_dir, ctx=mx.cpu())
+    return net
+
+
+def transform_fn(net, data, input_content_type, output_content_type):
+    """
+    Transform a request using the Gluon model. Called once per request.
+
+    :param net: The Gluon model.
+    :param data: The request payload.
+    :param input_content_type: The request content type.
+    :param output_content_type: The (desired) response content type.
+    :return: response payload and content type.
+    """
+    # we can use content types to vary input/output handling, but
+    # here we just assume json for both
+    parsed = json.loads(data)
+    nda = mx.nd.array(parsed)
+    output = net(nda)
+    prediction = mx.nd.argmax(output, axis=1)
+    response_body = json.dumps(prediction.asnumpy().tolist()[0])
+    return response_body, output_content_type
