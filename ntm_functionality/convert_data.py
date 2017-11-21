bd39222ccf7a0343dba49604e9c7ae22ee9c6ebc
djarpin <arpin@amazon.com>
2017-11-20 21:52:18 -0800

    Updated: convert_data script in NTM, fixed bad copy

--- ntm_functionality/convert_data.py
+++ ntm_functionality/convert_data.py
@@ -1,20 +1,42 @@
 import struct
 import io
 import boto3
+import sys
 
 import AmazonAIAlgorithmsIO_pb2
+from record_pb2 import Record
 
 
 def write_recordio(f, data):
-     kmagic = 0xced7230a
-     length = len(data)
-     f.write(struct.pack('I', kmagic))
-     f.write(struct.pack('I', length))
-     upper_align = ((length + 3) >> 2) << 2
-     padding = bytes([0x00 for _ in range(upper_align - length)])
-     f.write(data)
-     f.write(padding)
-  
+    kmagic = 0xced7230a
+    length = len(data)
+    f.write(struct.pack('I', kmagic))
+    f.write(struct.pack('I', length))
+    upper_align = ((length + 3) >> 2) << 2
+    padding = bytes([0x00 for _ in range(upper_align - length)])
+    f.write(data)
+    f.write(padding)
+
+
+def list_to_record_bytes(values, keys=None, label=None, feature_size=None):
+    record = Record()
+
+    record.features['values'].float32_tensor.values.extend(values)
+
+    if keys is not None:
+        if feature_size is None:
+            raise ValueError("For sparse tensors the feature size must be specified.")
+
+        record.features['values'].float32_tensor.keys.extend(keys)
+
+    if feature_size is not None:
+        record.features['values'].float32_tensor.shape.extend([feature_size])
+
+    if label is not None:
+        record.label['values'].float32_tensor.values.extend([label])
+
+    return record.SerializeToString()
+
 
 def read_next(f):
      kmagic = 0xced7230a
