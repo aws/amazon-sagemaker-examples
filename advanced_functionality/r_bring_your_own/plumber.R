7066c3977ac3c977385d3560f879ecd4b3ed0351
djarpin <arpin.david@gmail.com>
2017-11-25 21:27:05 -0800

    Update plumber.R

--- advanced_functionality/r_bring_your_own/plumber.R
+++ advanced_functionality/r_bring_your_own/plumber.R
@@ -1,5 +1,5 @@
 # plumber.R
-# Copyright [first edit year]-[latest edit year] Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 # 
