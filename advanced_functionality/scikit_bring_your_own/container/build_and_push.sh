534971c42e71529992311bf5d879429b9b9770c0
jesterhazy <jonathan.esterhazy@gmail.com>
2017-11-26 15:32:41 -0800

    update urls (#64)

--- advanced_functionality/scikit_bring_your_own/container/build_and_push.sh
+++ advanced_functionality/scikit_bring_your_own/container/build_and_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script shows how to build the Docker image and push it to ECR to be ready for use
-# by IM.
+# by SageMaker.
 
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
