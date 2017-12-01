69b97e02b2508679878ab4dd905e612fb12aed02
Ragav Venkatesan <email@ragav.net>
2017-12-01 11:42:02 -0800

    Removed eval methods.

--- under_development/tensorflow_iris_byom/iris_dnn_classifier.py
+++ under_development/tensorflow_iris_byom/iris_dnn_classifier.py
@@ -2,8 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-INPUT_TENSOR_NAME = 'x'
-
+INPUT_TENSOR_NAME = 'inputs'
 
 def estimator_fn(run_config, params):
     feature_columns = [tf.feature_column.numeric_column(INPUT_TENSOR_NAME, shape=[4])]
@@ -12,21 +11,13 @@ def estimator_fn(run_config, params):
                                       n_classes=3,
                                       config=run_config)
 
-
 def serving_input_fn():
     feature_spec = {INPUT_TENSOR_NAME: tf.FixedLenFeature(dtype=tf.float32, shape=[4])}
     return tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)()
 
-
 def train_input_fn(training_dir, params):
     """Returns input function that would feed the model during training"""
     return _generate_input_fn(training_dir, 'iris_training.csv')
-
-
-def eval_input_fn(training_dir, params):
-    """Returns input function that would feed the model during evaluation"""
-    return _generate_input_fn(training_dir, 'iris_test.csv')
-
 
 def _generate_input_fn(training_dir, training_filename):
     training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
