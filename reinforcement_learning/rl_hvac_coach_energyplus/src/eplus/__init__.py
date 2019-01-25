6e2ede7b2ed4fac84ef426e0751943dc75b4b549
Bharath Balaji <bhabalaj@amazon.com>
2019-01-25 12:06:08 -0800

    fixing num of steps in tsp easy and env entry point in hvac

--- reinforcement_learning/rl_hvac_coach_energyplus/src/eplus/__init__.py
+++ reinforcement_learning/rl_hvac_coach_energyplus/src/eplus/__init__.py
@@ -2,10 +2,10 @@ from gym.envs.registration import register
 
 register(
     id='large-office-v0',
-    entry_point='eplus.envs:LargeOffice',
+    entry_point='eplus.envs:LargeOfficeEnv',
 )
 
 register(
     id='data-center-v0',
-    entry_point='eplus.envs:DataCenter'
+    entry_point='eplus.envs:DataCenterEnv'
 )
