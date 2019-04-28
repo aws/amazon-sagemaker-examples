e6dfbd08c78aaca860091c918430bc3767368b9d
Yuval Fernbach <fernbach@amazon.com>
2019-04-28 18:13:22 +0300

    Bug fix

--- sagemaker-python-sdk/keras_script_mode_pipe_mode_horovod/generate_cifar10_tfrecords.py
+++ sagemaker-python-sdk/keras_script_mode_pipe_mode_horovod/generate_cifar10_tfrecords.py
@@ -1,23 +1,15 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#     Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#     Licensed under the Apache License, Version 2.0 (the "License").
+#     You may not use this file except in compliance with the License.
+#     A copy of the License is located at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         https://aws.amazon.com/apache-2-0/
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Read CIFAR-10 data from pickled numpy arrays and writes TFRecords.
-
-Generates tf.train.Example protos and writes them to TFRecord files from the
-python version of the CIFAR-10 dataset downloaded from
-https://www.cs.toronto.edu/~kriz/cifar.html.
-"""
+#     or in the "license" file accompanying this file. This file is distributed
+#     on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#     express or implied. See the License for the specific language governing
+#     permissions and limitations under the License.
 
 from __future__ import absolute_import
 from __future__ import division
@@ -109,7 +101,7 @@ def main(data_dir):
   print('Done!')
   import shutil
   shutil.rmtree(data_dir+'/cifar-10-batches-py')
-  os.remove(data_dir+'/cifar-10-python.tar.gz')
+  os.remove(data_dir+'/cifar-10-python.tar.gz') # Remove the original .tzr.gz files
 
 
 if __name__ == '__main__':
