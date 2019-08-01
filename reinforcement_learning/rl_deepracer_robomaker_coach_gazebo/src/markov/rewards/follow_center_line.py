cbb679649fb93609756c06c9b16cfdf40161bfe8
sunil19m <sunil19m@gmail.com>
2019-08-01 10:40:21 -0700

    Removing outdated reward function (#829)
    * With many changes to the SimulationApplication the notebook version of the DeepRacer became outdated.
    This did not work with new SimApp application
    
    Changes made to the notebook to work
    1. The notebook uses the custom docker instead of default sagemaker docker
    2. The files are made in consistence with the AwsSilverstoneMarkov Package. Going forward change to these files must be tested in the notebook aswell.
    3. S3FullAccess role was required to sagemaker role
    4. Fixed the issue with the evaluation worker import issues
    5. Metrics now uses the json file instead of the .csv file to show training rewards
    6. Added all the instructions to clean up the job
    7. More instructions are added on how to use the github README.md
    
    Testing:
    Tested it murasuni account, science-team account and burner account. It works.
    
    * Notebook support to new SimulationApplication
    
    Minor review comment fixes
    1. /common was symbolic link. Kept it the same and made modification to the ../common (original file)
    2. Removed all the commented code in the Dockerfile
    
    Testing:
    Tested it murasuni account, science-team account and burner account. It works.
    
    * Upgrading pandas and futures to fix the support for rl-coach
    
    * Removing outdated reward function
    
    1. These reward functions were used during the reInvent-2018 and are no more supported. Hence these reward functions are removed.
    2. Adding the 3 reward functions present in the console to this notebook
    3. Removing some of the unnecessary comments
    
    Tested:
    Tested locally

---
+++ reinforcement_learning/rl_deepracer_robomaker_coach_gazebo/src/markov/rewards/follow_center_line.py
@@ -0,0 +1,25 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+    
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
+    
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
+    
+    return float(reward)
