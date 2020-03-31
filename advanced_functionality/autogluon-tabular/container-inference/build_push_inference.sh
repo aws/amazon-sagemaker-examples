dc1623e0019de708cd0588e73dd94d88fd220d54
yohei <ec2-user@ip-172-16-6-76.us-east-2.compute.internal>
2020-03-31 05:16:22 +0000

    added autogluon-tabular example

---
+++ advanced_functionality/autogluon-tabular/container-inference/build_push_inference.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The name of our algorithm
+algorithm_name=${1:-sagemaker-autogluon-inference}
+
+# Get account
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration
+region=$(aws configure get region)
+region=${region:-us-east-2}
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --region ${region} --no-include-email)
+$(aws ecr get-login --registry-ids 763104351884 --region ${region} --no-include-email)
+    
+# Build the docker image, tag with full name and then push it to ECR
+docker build  -t ${algorithm_name} -f container-inference/Dockerfile.inference .
+docker tag ${algorithm_name} ${fullname}
+docker push ${fullname}
