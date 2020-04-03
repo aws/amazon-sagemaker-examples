cd167ea44b4c69a64bd0f224656d9bbc16d8c37f
EC2 Default User <ec2-user@ip-172-16-37-177.ec2.internal>
2020-04-03 21:53:40 +0000

    changed BYOM pytorch

--- sagemaker-ngc-examples/Pytorch BYOM from NGC/transform_script_hub.py
+++ sagemaker-ngc-examples/Pytorch BYOM from NGC/transform_script_hub.py
@@ -4,4 +4,4 @@ import os
 
 def model_fn(model_dir):
     model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_ssd', model_math='fp32',map_location='cpu')
-    return model
+    return model.to('cpu')
