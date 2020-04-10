842f35a3826d2a8a7073296a671ace3e2042abd5
yoheigon <yoheigon4@gmail.com>
2020-04-10 10:26:59 +0000

    add region support

--- advanced_functionality/autogluon-tabular/container-training/build_push_training.sh
+++ advanced_functionality/autogluon-tabular/container-training/build_push_training.sh
@@ -1,29 +1,33 @@
-#!/bin/bash
-
-# The name of our algorithm
-algorithm_name=${1:-sagemaker-autogluon-training}
+B#!/bin/bash
 
 # Get account
-account=$(aws sts get-caller-identity --query Account --output text)
+account=${1}
 
 # Get the region defined in the current configuration
-region=$(aws configure get region)
-region=${region:-us-east-1}
+region=${2:-us-east-1}
 
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+# The name of our algorithm
+algorithm_name=${3:-sagemaker-autogluon-training}
+
+uri_prefix=${4}
+fullname="${uri_prefix}/${algorithm_name}:latest"
+
+# Get the registry id
+registry_id=${5}
 
 # If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+aws ecr describe-repositories --region ${region} --repository-names "${algorithm_name}" > /dev/null 2>&1
 if [ $? -ne 0 ]
-then
-    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
-fi
+    then
+        aws ecr create-repository --region ${region} --repository-name "${algorithm_name}" > /dev/null
+	fi
 
 # Get the login command from ECR and execute it directly
 $(aws ecr get-login --region ${region} --no-include-email)
-$(aws ecr get-login --registry-ids 763104351884 --region ${region} --no-include-email)
+$(aws ecr get-login --registry-ids ${registry_id} --region ${region} --no-include-email)
 
 # Build the docker image, tag with full name and then push it to ECR
-docker build -t ${algorithm_name} -f container-training/Dockerfile.training . --build-arg REGION=${region}
+docker build -t ${algorithm_name} -f container-training/Dockerfile.training . --build-arg REGION=${region} --build-arg REGISTRY_ID=$\
+{registry_id}
 docker tag ${algorithm_name} ${fullname}
 docker push ${fullname}
