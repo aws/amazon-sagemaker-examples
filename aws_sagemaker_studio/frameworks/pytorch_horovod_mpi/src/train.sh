e2669840908c04c81dc9f2f6e54b1e7e9ada07f1
Balaji Veeramani <bvveeram@amazon.com>
2020-06-17 17:38:17 +0000

    Address commends

--- aws_sagemaker_studio/frameworks/pytorch_horovod_mpi/src/train.sh
+++ aws_sagemaker_studio/frameworks/pytorch_horovod_mpi/src/train.sh
@@ -2,4 +2,4 @@ mpirun -np 4 \
     -bind-to none -map-by slot \
     -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
     -mca pml ob1 -mca btl ^openib \
-    python train.py
+    python train.py "$@"
