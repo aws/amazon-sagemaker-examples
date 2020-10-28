5e843562d15d3a215dff90437453d35f13471a2a
minlu1021 <ec2-user@ip-172-16-72-244.us-west-2.compute.internal>
2020-10-28 17:50:51 +0000

    resolve comments

--- sagemaker_neo_compilation_jobs/deploy_pytorch_model_on_Inf1_instance/resnet18.py
+++ sagemaker_neo_compilation_jobs/deploy_pytorch_model_on_Inf1_instance/resnet18.py
@@ -1,46 +1,16 @@
-def neo_preprocess(payload, content_type):
-    import PIL.Image   # Training container doesn't have this package
-    import logging
-    import numpy as np
+def input_fn(request_body, request_content_type):
+    import torch
+    import torchvision.transforms as transforms
+    from PIL import Image
     import io
-
-    logging.info('Invoking user-defined pre-processing function')
-
-    if content_type != 'application/x-image':
-        raise RuntimeError('Content type must be application/x-image')
-
-    f = io.BytesIO(payload)
-    # Load image and convert to RGB space
-    image = PIL.Image.open(f).convert('RGB')
-    # Resize
-    image = np.asarray(image.resize((224, 224)))
-
-    # Normalize
-    mean_vec = np.array([0.485, 0.456, 0.406])
-    stddev_vec = np.array([0.229, 0.224, 0.225])
-    image = (image/255- mean_vec)/stddev_vec
-
-    # Transpose
-    if len(image.shape) == 2:  # for greyscale image
-        image = np.expand_dims(image, axis=2)
-    
-    image = np.rollaxis(image, axis=2, start=0)[np.newaxis, :]
-    
-    return image
-
-def neo_postprocess(result):
-    import logging
-    import numpy as np
-    import json
-
-    logging.info('Invoking user-defined post-processing function')
-
-    # Softmax (assumes batch size 1)
-    result = np.squeeze(result)
-    result_exp = np.exp(result - np.max(result))
-    result = result_exp / np.sum(result_exp)
-
-    response_body = json.dumps(result.tolist())
-    content_type = 'application/json'
-
-    return response_body, content_type
+    f = io.BytesIO(request_body)
+    input_image = Image.open(f).convert('RGB')
+    preprocess = transforms.Compose([
+      transforms.Resize(255),
+      transforms.CenterCrop(224),
+      transforms.ToTensor(),
+      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+    return input_batch
