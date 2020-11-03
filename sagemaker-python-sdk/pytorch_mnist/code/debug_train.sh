3314f8e90c7f984993d1c87c7811517284b2389c
Hongshan Li <hongshal@amazon.com>
2020-11-03 13:43:50 -0800

    added a debug file

--- sagemaker-python-sdk/pytorch_mnist/code/debug_train.sh
+++ sagemaker-python-sdk/pytorch_mnist/code/debug_train.sh
@@ -2,8 +2,8 @@
 
 export SM_HOSTS='0'
 export SM_CURRENT_HOST='0'
-export SM_MODEL_DIR='tmp/model'
-export SM_CHANNEL_TRAINING='tmp/data'
+export SM_MODEL_DIR='/tmp/model'
+export SM_CHANNEL_TRAINING='/tmp/data'
 export SM_NUM_GPUS='0'
 
 python train.py --epoch 0 --debug
