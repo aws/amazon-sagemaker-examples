fff69b205ebfc5c1aa298490fa48c48718a2087d
Hongshan Li <hongshal@amazon.com>
2020-11-19 03:31:29 +0000

    minor fix

--- frameworks/mxnet/utils.py
+++ frameworks/mxnet/utils.py
@@ -5,7 +5,10 @@ import os
 import boto3
 import json
 
-CONFIG_FILE="../../config.json"
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(dirname, "code/config.json"), "r") as f:
+    CONFIG = json.load(f)
 
 with open(CONFIG_FILE, "r") as f:
     CONFIG = json.load(f)
