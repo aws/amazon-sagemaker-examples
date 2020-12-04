9d11763ab73fd89a6d27df26288782bdbab4e180
Anirudh Subramanian <anirudh2290@ufl.edu>
2020-12-04 18:16:49 +0000

    Make license and copyright modification changes

--- training/distributed_training/pytorch/model_parallel/bert/bert_example/sagemaker_smp_pretrain.py
+++ training/distributed_training/pytorch/model_parallel/bert/bert_example/sagemaker_smp_pretrain.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # Copyright (c) 2019 NVIDIA CORPORATION. All rights reserved.
 # Copyright 2018 The Google AI Language Team Authors and The HugginFace Inc. team.
-# Modifications Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Modifications Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
