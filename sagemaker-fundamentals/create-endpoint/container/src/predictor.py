6d6188efb232e45dea6b47efd453587e335ec77c
Hongshan Li <hongshal@amazon.com>
2021-03-06 02:27:10 +0000

    more explanation on serving stack

---
+++ sagemaker-fundamentals/create-endpoint/container/src/predictor.py
@@ -0,0 +1,41 @@
+# This is the file that implements a flask server to do inferences. It's the file that you will modify to adapt to your own inference logic.
+
+import flask
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    """Determine if the container is working and healthy. 
+    In this sample container, we declare
+    it healthy if we can load the model successfully."""
+
+    status = 200
+    return flask.Response(response='\n', 
+        status=status, mimetype='application/json')
+
+@app.route('/invocations', methods=['POST'])
+def inference():
+    """Do an inference on incoming data. 
+    In this sample server, we take data as application/json,
+    print it out to confirm that the server received it.  
+    """
+    content_type = flask.request.content_type
+    if flask.request.content_type != "application/json":
+        msg = "I just take json, and I am fed with {}".format(
+            content_type)
+    else:
+        msg = "I am fed with json. Therefore, I am happy"
+
+    print("== The entire request object ==")
+    print(request)
+    
+    # define response header 
+    header = {} 
+    return flask.Response(
+        response=msg,
+        status=200,
+        header = header,
+        mimetype='text/plain')
+
