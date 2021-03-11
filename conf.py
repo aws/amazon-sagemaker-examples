09d3f93aa0be3723f659e12ce22baa5bcf8d35cb
Hongshan Li <hongshal@amazon.com>
2021-03-11 01:03:21 +0000

    rolled back some changes in conf.py

--- conf.py
+++ conf.py
@@ -1,77 +1,67 @@
-#Copyright 2017-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Configuration file for the Sphinx documentation builder.
 #
-# Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License. A copy of
-# the License is located at
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#     http://aws.amazon.com/apache2.0/
-#
-# or in the "license" file accompanying this file. This file is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
-# ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-"""Placeholder docstring"""
-from __future__ import absolute_import
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-import pkg_resources
-from datetime import datetime
 
-project = u"sagemaker"
-version = '0.0.0'
+# -- Project information -----------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+project = 'Amazon SageMaker Examples'
+copyright = '2020, Amazon Web Services, Inc. or its affiliates. All rights reserved.'
+author = 'Aaron Markham'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosectionlabel",
+    "nbsphinx",
 ]
 
+# extension nbsphinx config
+nbsphinx_execute = 'never'
+
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
-source_suffix = ".rst"  # The suffix of source filenames.
-master_doc = "index"  # The master toctree document.
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-copyright = u"%s, Amazon" % datetime.now().year
 
-# The full version, including alpha/beta/rc tags.
-release = version
+# -- Options for HTML output -------------------------------------------------
 
-# List of directories, relative to source directory, that shouldn't be searched
-# for source files.
-exclude_trees = ["_build"]
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
 
-pygments_style = "default"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
 
-autoclass_content = "both"
-autodoc_default_flags = ["show-inheritance", "members", "undoc-members"]
-autodoc_member_order = "bysource"
-
-html_theme = "sphinx_rtd_theme"
-
-html_static_path = ["_static"]
-
-htmlhelp_basename = "%sdoc" % project
-
-# files in _static/ to include
-# omit _static/
+# For Adobe Analytics
 html_js_files = [
     "https://a0.awsstatic.com/s_code/js/3.0/awshome_s_code.js", 
     "aws-ux-shortbread/index.js",
     "aws-ux-shortbread/init.js"
-]
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"http://docs.python.org/": None}
-
-# autosummary
-autosummary_generate = True
-
-# autosectionlabel
-autosectionlabel_prefix_document = True
+    ]
