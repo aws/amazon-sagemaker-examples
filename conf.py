5143be02d0f76649dbb4309e7e14de92b97ceb9d
Hongshan Li <lihongshan8128@gmail.com>
2021-03-10 17:05:42 -0800

    updated to s_code3 with shortbread (#2068)
    * updated to s_code3 with shortbread
    
    * rolled back some changes in conf.py

--- conf.py
+++ conf.py
@@ -60,4 +60,8 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # For Adobe Analytics
-html_js_files = ["https://a0.awsstatic.com/s_code/js/3.0/awshome_s_code.js", "js/analytics.js"]
+html_js_files = [
+    "https://a0.awsstatic.com/s_code/js/3.0/awshome_s_code.js", 
+    "aws-ux-shortbread/index.js",
+    "aws-ux-shortbread/init.js"
+    ]
