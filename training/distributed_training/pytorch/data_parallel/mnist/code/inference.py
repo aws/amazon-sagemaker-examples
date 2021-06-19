a3fba13d109a355300c5ab3723f417b378847ffd
Hongshan Li <hongshal@amazon.com>
2021-06-19 01:24:37 +0000

    fix infer_pytorch.ipynb

--- training/distributed_training/pytorch/data_parallel/mnist/code/inference.py
+++ training/distributed_training/pytorch/data_parallel/mnist/code/inference.py
@@ -18,19 +18,62 @@
 from __future__ import print_function
 
 import os
+import logging
+import json
+import re
+import sys
 
 import torch
 
 # Network definition
 from model_def import Net
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def model_fn(model_dir):
     print("In model_fn. Model directory is -")
     print(model_dir)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = Net()
+    clean_state_dict = {}
     with open(os.path.join(model_dir, "model.pth"), "rb") as f:
         print("Loading the mnist model")
-        model.load_state_dict(torch.load(f, map_location=device))
+        ckpt = torch.load(f, map_location=device)
+        
+        # remove module prefix from the key caused by torch.nn.DataParallel
+        # https://discuss.pytorch.org/t/solved-keyerror-unexpected-key-module-encoder-embedding-weight-in-state-dict/1686/8
+        for k, v in ckpt.items():
+            k = re.sub(r'module\.', '', k)
+            clean_state_dict[k] = v
+        model.load_state_dict(clean_state_dict)
     return model
+
+
+# data preprocessing
+def input_fn(request_body, request_content_type):
+    assert request_content_type == "application/json"
+    data = json.loads(request_body)["inputs"]
+    data = torch.tensor(data, dtype=torch.float32, device=device)
+    return data
+
+
+# inference
+def predict_fn(input_object, model):
+    with torch.no_grad():
+        prediction = model(input_object)
+    return prediction
+
+
+# postprocess
+def output_fn(predictions, content_type):
+    assert content_type == "application/json"
+    res = predictions.cpu().numpy().tolist()
+    return json.dumps(res)
+
+
+if __name__ == '__main__':
+    model = model_fn("../")
