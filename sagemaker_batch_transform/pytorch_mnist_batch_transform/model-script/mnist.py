b60606ccb8687fd0822345040944870da5689420
RamVegiraju <rav3sa@virginia.edu>
2021-07-22 16:44:06 -0400

    Fixing mnist dataset loading script pytorch batch example

--- sagemaker_batch_transform/pytorch_mnist_batch_transform/model-script/mnist.py
+++ sagemaker_batch_transform/pytorch_mnist_batch_transform/model-script/mnist.py
@@ -45,6 +45,7 @@ def _get_train_data_loader(batch_size, training_dir, is_distributed, **kwargs):
     logger.info("Get train data loader")
     dataset = datasets.MNIST(
         training_dir,
+        download=True,
         train=True,
         transform=transforms.Compose(
             [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
@@ -287,3 +288,4 @@ if __name__ == "__main__":
     parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
 
     train(parser.parse_args())
+
