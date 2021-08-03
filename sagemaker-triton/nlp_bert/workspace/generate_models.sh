77a0326014aa06a56b5dc09c2dff3ea90b0b265a
Aaqib Ansari <maaquib@gmail.com>
2021-08-03 14:47:21 -0700

    Adding nlp bert example files

---
+++ sagemaker-triton/nlp_bert/workspace/generate_models.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python onnx_exporter.py
+python pt_exporter.py
+trtexec --onnx=model.onnx --saveEngine=model_bs16.plan --minShapes=token_ids:1x128,attn_mask:1x128 --optShapes=token_ids:16x128,attn_mask:16x128 --maxShapes=token_ids:128x128,attn_mask:128x128 --fp16 --verbose --workspace=14000 | tee conversion_bs16_dy.txt
