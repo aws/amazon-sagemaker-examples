facb2fd2883ef7b5adc79d4cb3d81b63c7ff7463
Aaqib Ansari <maaquib@gmail.com>
2021-08-05 15:05:00 -0700

    Changes for BERT and updating Triton to 21.07

--- sagemaker-triton/nlp_bert/workspace/generate_models.sh
+++ sagemaker-triton/nlp_bert/workspace/generate_models.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python -m pip install transformers==4.9.1
 python onnx_exporter.py
 python pt_exporter.py
 trtexec --onnx=model.onnx --saveEngine=model_bs16.plan --minShapes=token_ids:1x128,attn_mask:1x128 --optShapes=token_ids:16x128,attn_mask:16x128 --maxShapes=token_ids:128x128,attn_mask:128x128 --fp16 --verbose --workspace=14000 | tee conversion_bs16_dy.txt
