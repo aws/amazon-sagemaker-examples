27dd72e9a038783fa2a8ce5785533f829b97780d
Massimiliano Angelino <angmas@amazon.com>
2021-08-12 09:47:04 +0000

    Example using SM Edge Manager and Greengrass
    - added Greengrass component
    - correct inference code to work with MobileNet v2

--- sagemaker_edge_manager/sagemaker_edge_example/inference.py
+++ sagemaker_edge_manager/sagemaker_edge_example/inference.py
@@ -3,6 +3,8 @@ from PIL import Image
 import agent_pb2
 import agent_pb2_grpc
 import os
+import numpy as np
+import json
 
 
 model_path = os.environ['MODEL_PATH'] 
@@ -46,7 +48,14 @@ def unload_model(name):
 
 def predict_image(model_name, image_path):
     image_tensor = agent_pb2.Tensor()
-    image_tensor.byte_data = Image.open(image_path).tobytes()
+    im = Image.open(image_path)
+    img = np.asarray(im)
+    # Neo compiled model requires the array to be of shape (3, 244, 244)
+    img = img.transpose(2,0,1)
+    # normalization according to https://github.com/tensorflow/tensorflow/blob/a4dfb8d1a71385bd6d122e4f27f86dcebb96712d/tensorflow/python/keras/applications/imagenet_utils.py#L259
+    img = (x/127.5).astype(np.float32)
+    img -= 1.
+    image_tensor.byte_data = img.tobytes()
     image_tensor_metadata = list_model_tensors(list_models())[model_name]['inputs'][0]
     image_tensor.tensor_metadata.name = image_tensor_metadata.name
     image_tensor.tensor_metadata.data_type = image_tensor_metadata.data_type
@@ -85,7 +94,17 @@ def main():
 
     print('Predict')
     try: 
-        predict_image(ml_model, image_path=image_path)
+        prediction = predict_image(ml_model, image_path=image_path)
+        #print(prediction) # uncomment to print the predictio object
+        pred = np.frombuffer(prediction.tensors[0].byte_data, dtype = np.float32)
+        # the returned array has shape (1000,), while mobilenet v2 returns a shape (1, 1000)
+        # decoding results https://github.com/tensorflow/tensorflow/blob/a4dfb8d1a71385bd6d122e4f27f86dcebb96712d/tensorflow/python/keras/applications/imagenet_utils.py#L159
+        
+        top_indexes = pred.argsort()[-5:][::-1]
+        with open(os.environ["IMAGENET_CLASS_INDEX_PATH"]) as f:
+            classes = json.load(f)
+        result = [tuple(classes[str(i)]) + (pred[i],) for i in top_indexes]
+        print(result)
     except Exception as e:
         print()
         print(e)
