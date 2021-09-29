7afcb2b2f36c747fb9221f07d6206eedc7722dbf
Alexander Shirkov <ashyrkou@amazon.com>
2021-09-29 11:46:56 -0700

    AutoGluon deep learning containers

--- advanced_functionality/autogluon-tabular-containers/ag_model.py
+++ advanced_functionality/autogluon-tabular-containers/ag_model.py
@@ -6,20 +6,28 @@ from sagemaker import utils
 from sagemaker.serializers import CSVSerializer
 from sagemaker.deserializers import StringDeserializer
 
+
 ACCOUNT = 763104351884
 ECR_TRAINING_REPO = "autogluon-training"
 ECR_INFERENCE_REPO = "autogluon-inference"
-AG_VERSION = "0.3.1"
-TRAINING_IMAGE_CPU = f"{AG_VERSION}-cpu-py37-ubuntu18.04"
-TRAINING_IMAGE_GPU = f"{AG_VERSION}-gpu-py37-cu102-ubuntu18.04"
-INFERENCE_IMAGE_CPU = f"{AG_VERSION}-cpu-py37-ubuntu16.04"
+TRAINING_IMAGE_CPU = "cpu-py37-ubuntu18.04"
+TRAINING_IMAGE_GPU = "gpu-py37-cu102-ubuntu18.04"
+INFERENCE_IMAGE_CPU = "cpu-py37-ubuntu16.04"
 
 
 class AutoGluonTraining(Framework):
     def __init__(
-        self, entry_point, region, image_type="cpu", source_dir=None, hyperparameters=None, **kwargs
+        self,
+        entry_point,
+        region,
+        framework_version,
+        image_type="cpu",
+        source_dir=None,
+        hyperparameters=None,
+        **kwargs,
     ):
         image = TRAINING_IMAGE_GPU if image_type == "gpu" else TRAINING_IMAGE_CPU
+        image = f"{framework_version}-{image}"
         image_uri = f"{ACCOUNT}.dkr.ecr.{region}.amazonaws.com/{ECR_TRAINING_REPO}:{image}"
         super().__init__(entry_point, source_dir, hyperparameters, image_uri=image_uri, **kwargs)
 
@@ -48,10 +56,9 @@ class AutoGluonTabularPredictor(Predictor):
 
 
 class AutoGluonInferenceModel(MXNetModel):
-    def __init__(self, model_data, role, entry_point, region, **kwargs):
-        image_uri = (
-            f"{ACCOUNT}.dkr.ecr.{region}.amazonaws.com/{ECR_INFERENCE_REPO}:{INFERENCE_IMAGE_CPU}"
-        )
+    def __init__(self, model_data, role, entry_point, region, framework_version, **kwargs):
+        image = f"{framework_version}-{INFERENCE_IMAGE_CPU}"
+        image_uri = f"{ACCOUNT}.dkr.ecr.{region}.amazonaws.com/{ECR_INFERENCE_REPO}:{image}"
         super().__init__(
             model_data, role, entry_point, image_uri=image_uri, framework_version="1.8.0", **kwargs
         )
