135cb9ea80c4befedfaa4f305aab587c777aa9d6
Aaron Markham <markhama@amazon.com>
2021-11-02 10:52:02 -0700

    remove old example; ref #V461586355

--- advanced_functionality/distributed_tensorflow_mask_rcnn/container-serving-optimized/build_tools/set_env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export IMAGE_NAME=mask-rcnn-tensorflow-serving-sagemaker
-export IMAGE_TAG=tf1.15-99dda64
--- advanced_functionality/distributed_tensorflow_mask_rcnn/container-serving/build_tools/set_env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export IMAGE_NAME=mask-rcnn-tensorpack-serving-sagemaker
-export IMAGE_TAG=tf1.15-tpdb541e8
