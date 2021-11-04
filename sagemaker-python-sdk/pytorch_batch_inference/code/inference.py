1dec3e434fadfadf30e717577db739e908d1347c
Nikhil Kulkarni <knikhil29@gmail.com>
2021-11-04 00:55:04 +0000

    Format using black -l 100

--- sagemaker-python-sdk/pytorch_batch_inference/code/inference.py
+++ sagemaker-python-sdk/pytorch_batch_inference/code/inference.py
@@ -8,7 +8,8 @@ from transformers import (
 )
 import os
 
-JSON_CONTENT_TYPE = 'application/json'
+JSON_CONTENT_TYPE = "application/json"
+
 
 def model_fn(model_dir):
     print("LOADING MODEL")
@@ -21,8 +22,8 @@ def model_fn(model_dir):
 
     model_name = setup_config.get("model_name")
 
-    model =  AutoModelForSequenceClassification.from_pretrained(model_dir)
-    tokenizer =  AutoTokenizer.from_pretrained(model_name ,do_lower_case= True)
+    model = AutoModelForSequenceClassification.from_pretrained(model_dir)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, do_lower_case=True)
     return (model, tokenizer, mapping_file_path)
 
 
@@ -34,28 +35,34 @@ def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
         return input_data
 
     else:
-        raise Exception('Requested unsupported ContentType in Accept: ' + content_type)
+        raise Exception("Requested unsupported ContentType in Accept: " + content_type)
         return
 
 
 def predict_fn(input_data, model_pack):
-    
-    print('Got input Data: {}'.format(input_data))
+
+    print("Got input Data: {}".format(input_data))
     model = model_pack[0]
     tokenizer = model_pack[1]
     mapping_file_path = model_pack[2]
-    
+
     with open(mapping_file_path) as f:
         mapping = json.load(f)
-   
-    inputs = tokenizer.encode_plus(input_data, max_length=128, pad_to_max_length=True, add_special_tokens=True, return_tensors='pt') 
-    output = model(inputs['input_ids'])
-    
+
+    inputs = tokenizer.encode_plus(
+        input_data,
+        max_length=128,
+        pad_to_max_length=True,
+        add_special_tokens=True,
+        return_tensors="pt",
+    )
+    output = model(inputs["input_ids"])
+
     print("PRED", output)
-    
+
     inferences = []
     num_rows, num_cols = output[0].shape
-    
+
     for i in range(num_rows):
         out = output[0][i].unsqueeze(0)
         y_hat = out.argmax(1).item()
@@ -69,8 +76,8 @@ def predict_fn(input_data, model_pack):
 
 def output_fn(prediction_output, accept=JSON_CONTENT_TYPE):
     print("PREDICTION", prediction_output)
-    
+
     if accept == JSON_CONTENT_TYPE:
         return json.dumps(prediction_output), accept
 
-    raise Exception('Requested unsupported ContentType in Accept: ' + accept)
+    raise Exception("Requested unsupported ContentType in Accept: " + accept)
