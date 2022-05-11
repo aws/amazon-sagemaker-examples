1e03a53eeb731e444274f5cd9ed22726d2a44941
Nikhil Kulkarni <knikhil29@gmail.com>
2022-05-11 19:03:14 +0000

    Add custom container build in SM PT Batch Inference

---
+++ sagemaker-python-sdk/pytorch_batch_inference/docker/dockerd-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if [[ "$1" = "serve" ]]; then
+    shift 1
+    printenv
+    ls /opt
+    torchserve --start --ts-config /home/model-server/config.properties --model-store /opt/ml/model --models all 
+     
+else
+    eval "$@"
+fi
+
+# prevent docker exit
+tail -f /dev/null
+
