a96b147a772cbf16655ddf40548ebec827357546
Joao Moura <joaopcm@amazon.es>
2023-01-17 14:48:59 +0000

    Removed text encoder compilation comment in TensorRT compilation script

--- sagemaker-triton/business_logic_scripting/stable_diffusion/export.sh
+++ sagemaker-triton/business_logic_scripting/stable_diffusion/export.sh
@@ -9,5 +9,3 @@ python export.py
 # Accelerating VAE with TensorRT
 trtexec --onnx=vae.onnx --saveEngine=vae.plan --minShapes=latent_sample:1x4x64x64 --optShapes=latent_sample:4x4x64x64 --maxShapes=latent_sample:8x4x64x64 --fp16
 
-#Accelearating text encoder with TensorRT 
-# trtexec --onnx=encoder.onnx --saveEngine=encoder.plan --minShapes=input_ids:1x77 --optShapes=input_ids:4x77 --maxShapes=input_ids:8x77 --fp16
