3328f1370ede059fb6125b7dd124c44239bdc407
Ryan Dana <ryandana@amazon.com>
2023-02-07 23:37:18 -0800

    fix: fix sagemaker example for multi-container-endpoint

--- advanced_functionality/multi-container-endpoint/direct-invocation/pytorch/code/inference.py
+++ advanced_functionality/multi-container-endpoint/direct-invocation/pytorch/code/inference.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import sys
+import os
 
 import torch
 import torch.nn as nn
@@ -35,6 +36,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def model_fn(model_dir):
     model = Net().to(device)
+    with open(os.path.join(model_dir, 'model.pth'), 'rb') as f:
+        model.load_state_dict(torch.load(f))
     model.eval()
     return model
 
@@ -48,9 +51,9 @@ def input_fn(request_body, request_content_type):
 
 
 # inference
-def predict_fn(input_object, model):
+def predict_fn(data, model):
     with torch.no_grad():
-        prediction = model(input_object)
+        prediction = model(data)
     return prediction
 
 
