47278df319a77fd31264f76a5279a75a978f1862
xieyongliang <yongliangxie@hotmail.com>
2023-04-19 11:21:34 +0800

    update train.py and serve

--- inference/generativeai/llm-workshop/lab7-hosting-controlnet-models-on-sagemaker/train.py
+++ inference/generativeai/llm-workshop/lab7-hosting-controlnet-models-on-sagemaker/train.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--train-args', type=str, help='Train arguments')
@@ -12,8 +13,9 @@ args = parser.parse_args()
 
 cmd = "LD_LIBRARY_PATH=/opt/conda/lib:$LD_LIBRARY_PATH ACCELERATE=true bash webui.sh --port 8080 --listen --xformers --train --lora-models-path /opt/ml/input/data/lora --dreambooth-models-path /opt/ml/input/data/dreambooth  --ckpt-dir /opt/ml/input/data/models --train-args '{0}' --sd-models-s3uri {1} --db-models-s3uri {2} --lora-models-s3uri {3} --dreambooth-config-id {4}".format(args.train_args, args.sd_models_s3uri, args.db_models_s3uri, args.lora_models_s3uri, args.dreambooth_config_id)
 
-os.system('mkdir -p /opt/ml/input/data/lora')
-os.system('mkdir -p /opt/ml/input/data/dreambooth')
-os.system('mkdir -p /opt/ml/input/data/models')
+os.makedirs(os.path.dirname("/opt/ml/input/data/lora"), exist_ok=True)
+os.makedirs(os.path.dirname("/opt/ml/input/data/dreambooth"), exist_ok=True)
+os.makedirs(os.path.dirname("/opt/ml/input/data/models"), exist_ok=True)
 
-os.system(cmd)
+ret = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+print(ret)
