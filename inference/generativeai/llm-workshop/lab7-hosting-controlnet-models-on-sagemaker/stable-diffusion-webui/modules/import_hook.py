690d5b494bfc294480f7060c92a47ef9f409668a
xieyongliang <yongliangxie@hotmail.com>
2023-04-19 08:35:18 +0800

    update Dockerfile.training and Dockerfile.inference and cleanup

--- inference/generativeai/llm-workshop/lab7-hosting-controlnet-models-on-sagemaker/stable-diffusion-webui/modules/import_hook.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-# this will break any attempt to import xformers which will prevent stability diffusion repo from trying to use it
-if "--xformers" not in "".join(sys.argv):
-    sys.modules["xformers"] = None
