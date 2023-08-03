00d5c8b802f6173148ce71b12a0075c1a7f8ca11
aaronkl <kleinaa@cs.uni-freiburg.de>
2023-08-03 11:25:56 +0200

    header

--- hyperparameter_tuning/neural_architecture_search_llm/evaluate_subnetwork.py
+++ hyperparameter_tuning/neural_architecture_search_llm/evaluate_subnetwork.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Copyright 2020 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-""" Finetuning the library models for sequence classification on GLUE."""
-import json
-
-# You can also adapt this script on your own text classification task. Pointers for this are left as comments.
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 import os
-import time
 import logging
 import sys
 
