4b711d57ed27713a013283862704c6b2cda1d4e5
Praveen Chamarthi <pchamart@amazon.com>
2023-08-07 16:46:36 -0700

    update wsgi.py to import preprocessing.py as app

--- inference/structured/realtime/byoc/byoc-nginx-python/featurizer/code/wsgi.py
+++ inference/structured/realtime/byoc/byoc-nginx-python/featurizer/code/wsgi.py
@@ -1,4 +1,4 @@
-import inference as myapp
+import preprocessing as myapp
 
 # This is just a simple wrapper for gunicorn to find your app.
 # If you want to change the algorithm file, simply change "predictor" above to the
