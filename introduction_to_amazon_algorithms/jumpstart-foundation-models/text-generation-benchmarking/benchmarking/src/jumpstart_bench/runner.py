b8e8085dbefee9cd553112c8f5046b44b7eba745
Kyle Ulrich <ulrichkr@amazon.com>
2024-03-10 21:52:23 +0000

    chore: revert raise error

--- introduction_to_amazon_algorithms/jumpstart-foundation-models/text-generation-benchmarking/benchmarking/src/jumpstart_bench/runner.py
+++ introduction_to_amazon_algorithms/jumpstart-foundation-models/text-generation-benchmarking/benchmarking/src/jumpstart_bench/runner.py
@@ -334,9 +334,8 @@ class Benchmarker:
                     endpoints[model_id] = predictor.endpoint_name
                     metrics.extend(metrics_model_id)
                 except Exception as e:
-                    raise e
-                    # errors[model_id] = e
-                    # logging.error(f"{logging_prefix(model_id)} Benchmarking failed: {e}")
+                    errors[model_id] = e
+                    logging.error(f"{logging_prefix(model_id)} Benchmarking failed: {e}")
 
         output = {
             "models": models,
