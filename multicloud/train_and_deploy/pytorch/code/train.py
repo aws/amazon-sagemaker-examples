562d046ccaa6238dea88d706fce3ff0702847b18
Raja Vaidyanathan <rajavaid@amazon.co.uk>
2024-04-16 16:25:17 +0000

    torch.inference_mode() instead of torch.no_grad()

--- multicloud/train_and_deploy/pytorch/code/train.py
+++ multicloud/train_and_deploy/pytorch/code/train.py
@@ -67,7 +67,7 @@ def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
-    with torch.no_grad():
+    with torch.inference_mode():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
