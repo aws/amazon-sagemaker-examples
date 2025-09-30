ffd9df0fd358cf820fc640fc6ebf13eb7c861774
Ajay Vohra <ajayvohr@amazon.co>
2025-09-30 10:18:03 -0400

    update containers/examples

--- advanced_functionality/large-model-inference-testing/examples/triton-inference-server/trtllm_backend/meta-llama3-8b-instruct/code/build_engine.sh
+++ advanced_functionality/large-model-inference-testing/examples/triton-inference-server/trtllm_backend/meta-llama3-8b-instruct/code/build_engine.sh
@@ -40,9 +40,10 @@ trtllm-build \
 mkdir -p $MODEL_REPO
 
 echo "Build Triton TensorRT-LLM model"
-cd /opt/TensorRT-LLM/triton_backend
+cd /opt/tensorrtllm_backend
 
 TOKENIZER_DIR=$MODEL_PATH
+TOKENIZER_TYPE=auto
 DECOUPLED_MODE=false
 INSTANCE_COUNT=1
 MAX_QUEUE_DELAY_MS=100
@@ -54,8 +55,8 @@ cp -r all_models/inflight_batcher_llm/postprocessing $MODEL_REPO/${MODEL_NAME}_p
 cp -r all_models/inflight_batcher_llm/tensorrt_llm_bls $MODEL_REPO/${MODEL_NAME}_tensorrt_llm_bls
 cp -r all_models/inflight_batcher_llm/ensemble $MODEL_REPO/${MODEL_NAME}
 cp -r all_models/inflight_batcher_llm/tensorrt_llm $MODEL_REPO/${MODEL_NAME}_tensorrt_llm
-python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}_preprocessing/config.pbtxt tokenizer_dir:${TOKENIZER_DIR},triton_max_batch_size:${MAX_NUM_SEQS},preprocessing_instance_count:${INSTANCE_COUNT} 
-python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}_postprocessing/config.pbtxt tokenizer_dir:${TOKENIZER_DIR},triton_max_batch_size:${MAX_NUM_SEQS},postprocessing_instance_count:${INSTANCE_COUNT}
+python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}_preprocessing/config.pbtxt tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:${TOKENIZER_TYPE},triton_max_batch_size:${MAX_NUM_SEQS},preprocessing_instance_count:${INSTANCE_COUNT}
+python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}_postprocessing/config.pbtxt tokenizer_dir:${TOKENIZER_DIR},tokenizer_type:${TOKENIZER_TYPE},triton_max_batch_size:${MAX_NUM_SEQS},postprocessing_instance_count:${INSTANCE_COUNT}
 python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}_tensorrt_llm_bls/config.pbtxt triton_max_batch_size:${MAX_NUM_SEQS},decoupled_mode:${DECOUPLED_MODE},bls_instance_count:${INSTANCE_COUNT},tensorrt_llm_model_name:${MODEL_NAME}_tensorrt_llm
 python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}/config.pbtxt triton_max_batch_size:${MAX_NUM_SEQS}
 python3 ${FILL_TEMPLATE_SCRIPT} -i ${MODEL_REPO}/${MODEL_NAME}_tensorrt_llm/config.pbtxt triton_max_batch_size:${MAX_NUM_SEQS},decoupled_mode:${DECOUPLED_MODE},engine_dir:${ENGINE_DIR},max_queue_delay_microseconds:${MAX_QUEUE_DELAY_MS},batching_strategy:inflight_fused_batching,encoder_input_features_data_type:${encoder_input_features_data_type},triton_backend:tensorrtllm
@@ -67,7 +68,5 @@ sed -i 's/name: "tensorrt_llm"/name: "model_tensorrt_llm"/1' ${MODEL_REPO}/${MOD
 sed -i 's/model_name: "preprocessing"/model_name: "model_preprocessing"/1' ${MODEL_REPO}/${MODEL_NAME}/config.pbtxt
 sed -i 's/model_name: "postprocessing"/model_name: "model_postprocessing"/1' ${MODEL_REPO}/${MODEL_NAME}/config.pbtxt
 sed -i 's/model_name: "tensorrt_llm"/model_name: "model_tensorrt_llm"/1' ${MODEL_REPO}/${MODEL_NAME}/config.pbtxt
-sed -i 's/${logits_datatype}/TYPE_FP32/g' ${MODEL_REPO}/${MODEL_NAME}/config.pbtxt
-sed -i 's/${logits_datatype}/TYPE_FP32/g' ${MODEL_REPO}/${MODEL_NAME}_tensorrt_llm/config.pbtxt
-sed -i 's/${logits_datatype}/TYPE_FP32/g' ${MODEL_REPO}/${MODEL_NAME}_tensorrt_llm_bls/config.pbtxt
 
+echo "Build engine script done!"
