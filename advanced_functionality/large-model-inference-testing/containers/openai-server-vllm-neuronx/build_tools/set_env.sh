d011f8d419c53ec8218b001ce5ce3b198db5b109
Ajay Vohra <ajayvohr@amazon.com>
2026-04-05 09:25:36 -0400

    update neuron sdk 2.28.0

--- advanced_functionality/large-model-inference-testing/containers/openai-server-vllm-neuronx/build_tools/set_env.sh
+++ advanced_functionality/large-model-inference-testing/containers/openai-server-vllm-neuronx/build_tools/set_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=openai-server-neuronx-vllm
-export IMAGE_TAG=2.27.1-0.13.0
+export IMAGE_TAG=2.28.0-0.13.0
--- advanced_functionality/large-model-inference-testing/containers/tritonserver-neuronx/build_tools/set_env.sh
+++ advanced_functionality/large-model-inference-testing/containers/tritonserver-neuronx/build_tools/set_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export IMAGE_NAME=tritonserver-neuronx
-export IMAGE_TAG=26.01-2.27.1
+export IMAGE_TAG=26.01-2.28.0
